db22e140dad6e6806a368cfe4fdd886f7b70ff49
Paola Aguilar <pao.aquev29@gmail.com>
2021-04-08 16:16:21 -0600

    adding lazy loading to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import Layout from './components/Layout';
-import AuthProvider from './components/context/AuthContext';
+import React, {lazy, Suspense} from 'react';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Login from './components/pages/Login';
-import Home from './components/pages/Home';
-import ListOfGames from './components/pages/ListOfGames';
-import GameDetail from './components/pages/GameDetail';
-import { ROUTE } from './components/types/routing';
+import { ROUTE } from './components/types/routing'
+import AuthProvider from './components/context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
+const Layout = lazy(() => import ('./components/Layout'));
+const Login = lazy(() => import ('./components/pages/Login'));
+const Home = lazy(()=> import ('./components/pages/Home'))
+const ListOfGames = lazy(() => import ('./components/pages/ListOfGames'))
+const GameDetail = lazy(()=> import ('./components/pages/GameDetail'))
 
 function App(): JSX.Element {
   return (
     <AuthProvider>
       <Router>
+        <Suspense fallback>
         <Layout>
           <Route exact path={ROUTE.LOGIN} component={Login} />
           <PrivateRoute exact path={ROUTE.HOME} component={Home} />
           <PrivateRoute exact path={ROUTE.LIST_OF_GAMES} component={ListOfGames} />
           <PrivateRoute exact path={ROUTE.GAME_DETAIL} component={GameDetail} />
         </Layout>
+        </Suspense>
       </Router>
     </AuthProvider>
   );
